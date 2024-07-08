6fa4c1267100c71de81791198a5d35c4680f4b20
Juan Pablo <95457934+SoriaJP@users.noreply.github.com>
2024-07-08 09:34:17 -0300

    actualizando useReducer

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,10 +1,6 @@
-import { act, useReducer } from "react";
+import { useReducer, useState } from "react";
 
-const initialState = {contador:0};
-
-const init = (initialState)=>{
-    return ({contador: initialState.contador + 100});
-};
+const intialState = {contador: 0};
 
 const TYPES = {
     INCREMENT:"INCREMENT",
@@ -14,43 +10,24 @@ const TYPES = {
     RESTART:"RESTART"
 }
 
-function reducer(state, action, init){
+function reducer(state,action){
     switch(action.type){
         case TYPES.INCREMENT:
-            return({contador:state.contador + 1});
+           return({contador: state.contador + 1});
         case TYPES.DECREMENT:
-            return({contador:state.contador - 1});
-        case TYPES.INCREMENT5:
-            return({contador:state.contador + action.payload});
+            return({contador: state.contador - 1});
         case TYPES.DECREMENT5:
-            return({contador:state.contador - action.payload});
+            return({contador: state.contador + 5});
+        case TYPES.INCREMENT5:
+            return({contador: state.contador - 5});
         case TYPES.RESTART:
-            return initialState;
+            return(intialState);
         default:
             return state;
     }
 }
 
 export default function Contador(){
-    const [state, dispatch] = useReducer(reducer,initialState)
 
-    const restar5 = ()=>dispatch({type:TYPES.DECREMENT5,payload:5})
-    const restar = ()=>dispatch({type:TYPES.DECREMENT})
-    const iniciar = ()=>dispatch({type:TYPES.RESTART})
-    const sumar = ()=>dispatch({type:TYPES.INCREMENT})
-    const sumar5 = ()=>dispatch({type:TYPES.INCREMENT5,payload:5})
-
-    return(
-        <div className="block">
-            <h2 className="title is-2">Contador con useReducer</h2>
-            <p className="title is-6">El valor actual del contador es {state.contador}</p>
-            <div className="block">
-                <button className="button is-primary" onClick={restar5}>Decrementar en 5</button>
-                <button className="button is-primary" onClick={restar}> Decrementar</button>
-                <button className="button is-primary" onClick={iniciar}>Inicializar</button>
-                <button className="button is-primary" onClick={sumar}>Incrementar</button>
-                <button className="button is-primary" onClick={sumar5}>Incrementar en 5</button>
-            </div>
-        </div>
-    );
+    const [state, dispatch] = useReducer(reducer,intialState)
 }
