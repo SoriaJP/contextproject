2f05c837ef71e96704c5fc5ed9e91d7888f21601
Juan Pablo <95457934+SoriaJP@users.noreply.github.com>
2024-07-09 17:43:26 -0300

    nueva rama para ver el tema formularios

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,18 @@ import SongList from "./components/SongList"
 import FooterBar from "./components/FooterBar";
 import NavBar from "./components/NavBar";
 import ChangeTheme from "./contexts/ThemeContext";
-import Contador from "./components/Contador";
+
 
 function App() {
 
     return(
         <>
-            <Contador />
-            {/*<ChangeTheme>
+            <ChangeTheme>
                 <NavBar />
                 <SongList />
                 <br/>
                 <FooterBar />
-            </ChangeTheme>*/}
+            </ChangeTheme>
         </>
     );
 
